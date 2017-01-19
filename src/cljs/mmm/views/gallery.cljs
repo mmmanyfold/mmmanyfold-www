8c8d003e10a084faef4183db4ae2bc1051f960fd
eemshi <oreneemshi@gmail.com>
2017-01-19 00:01:58 -0700

    adjusts styles and gallery items

--- src/cljs/mmm/views/gallery.cljs
+++ src/cljs/mmm/views/gallery.cljs
@@ -5,18 +5,12 @@
   [:div.gallery-wrap
 
     [:div.gallery-section
-      [:h2 [:mark "Client Sites"]]
+      [:h2 [:mark "Client Work"]]
 
       [:div.gallery-row
         [:div.gallery-item
-          [:img {:src "img/gallery/picture-room.png"}]
-          [:div.label "Picture Room"]]
-        [:div.gallery-item
           [:img {:src "img/gallery/playground.jpg"}]
           [:div.label "Playground Coffee Shop"]]
-        [:div.gallery-item
-          [:img {:src "img/gallery/dizzy.jpg"}]
-          [:div.label "Dizzy Magazine"]]
         [:div.gallery-item
           [:img {:src "img/gallery/sporting-life.gif"}]
           [:div.label "Sporting Life"]]
@@ -24,11 +18,17 @@
           [:img {:src "img/gallery/princess-nokia.jpg"}]
           [:div.label "Princess Nokia"]]
         [:div.gallery-item
+          [:img {:src "img/gallery/dizzy.jpg"}]
+          [:div.label "Dizzy Magazine"]]
+        [:div.gallery-item
+          [:img {:src "img/gallery/picture-room.png"}]
+          [:div.label "Picture Room"]]
+        [:div.gallery-item
           [:img {:src "img/gallery/letter-racer.gif"}]
           [:div.label "Letter Racer"]]]]
 
     [:div.gallery-section
-      [:h2 [:mark "Learning Design"]]
+      [:h2 [:mark "Educational Design"]]
 
       [:div.gallery-row
         [:div.gallery-item
@@ -36,7 +36,10 @@
           [:div.label "OWLET"]]
         [:div.gallery-item
           [:img {:src "img/gallery/design-challenge.gif"}]
-          [:div.label "Denver Art Museum"]]]]
+          [:div.label "Denver Art Museum"]]
+        [:div.gallery-item
+          [:img {:src "img/gallery/girls-in-stem.jpg"}]
+          [:div.label "Girls in STEM"]]]]
 
     [:div.gallery-section
       [:h2 [:mark "Talks + Performances"]]
