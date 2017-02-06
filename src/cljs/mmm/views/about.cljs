0ce045a839ec85b39b73c5bc89d0b5c1268a4e38
eemshi <oreneemshi@gmail.com>
2017-02-05 20:49:27 -0700

    adds links to gallery, img link hover effect, and 'coming soon' hover effect

--- src/cljs/mmm/views/about.cljs
+++ src/cljs/mmm/views/about.cljs
@@ -7,4 +7,4 @@
     [:p "We believe in amplifying "
       [:a {:href "https://www.ted.com/talks/alan_kay_shares_a_powerful_idea_about_ideas?language=en"}
         [:span.italic "powerful ideas"]] " through software."]
-    [:p [:b "mmmanyfold"] " takes inspiration from self-organizing systems such as the block chain, #blacklivesmatter, microservices & distributed systems, functional programming, the Fluxus movement, etc."]])
+    [:p [:b "mmmanyfold"] " takes inspiration from self-organizing systems such as the blockchain, #blacklivesmatter, microservices & distributed systems, functional programming, the Fluxus movement, etc."]])
--- src/cljs/mmm/views/gallery.cljs
+++ src/cljs/mmm/views/gallery.cljs
@@ -1,5 +1,8 @@
 (ns mmm.views.gallery
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent :refer [atom]]))
+
+(def visibility (reagent/atom "hidden"))
 
 (defn gallery-view []
   [:div.gallery-wrap
@@ -9,51 +12,66 @@
 
       [:div.gallery-row
         [:div.gallery-item
+         [:a {:href "http://playgroundcoffeeshop.com"}
           [:img {:src "img/gallery/playground.jpg"}]
-          [:div.label "Playground Coffee Shop"]]
+          [:div.label "Playground Coffee Shop"]]]
         [:div.gallery-item
+         [:a {:href "http://sportinglife.nyc"}
           [:img {:src "img/gallery/sporting-life.gif"}]
-          [:div.label "Sporting Life"]]
+          [:div.label "Sporting Life"]]]
         [:div.gallery-item
+         [:a {:href "http://princessnokia.org"}
           [:img {:src "img/gallery/princess-nokia.jpg"}]
-          [:div.label "Princess Nokia"]]
-        [:div.gallery-item
+          [:div.label "Princess Nokia"]]]
+        [:div.gallery-item {:on-mouse-enter #(reset! visibility "visible")
+                            :on-mouse-leave #(reset! visibility "hidden")}
+          [:div.soon {:style {:visibility @visibility}}
+           "Coming Soon"]
           [:img {:src "img/gallery/dizzy.jpg"}]
           [:div.label "Dizzy Magazine"]]
         [:div.gallery-item
+         [:a {:href "http://pictureroom.mcnallyjacksonstore.com"}
           [:img {:src "img/gallery/picture-room.png"}]
-          [:div.label "Picture Room"]]
+          [:div.label "Picture Room"]]]
         [:div.gallery-item
+         [:a {:href "http://letterracer.com"}
           [:img {:src "img/gallery/letter-racer.gif"}]
-          [:div.label "Letter Racer"]]]]
+          [:div.label "Letter Racer"]]]]]
 
     [:div.gallery-section
       [:h2 [:mark "Educational Design"]]
 
       [:div.gallery-row
         [:div.gallery-item
+         [:a {:href "http://owlet.codefordenver.org"}
           [:img {:src "img/gallery/owlet.png"}]
-          [:div.label "OWLET"]]
+          [:div.label "OWLET"]]]
         [:div.gallery-item
+         [:a {:href "http://www.mmmanyfold.com/design-challenge.html"}
           [:img {:src "img/gallery/design-challenge.gif"}]
-          [:div.label "Denver Art Museum"]]
+          [:div.label "Denver Art Museum"]]]
         [:div.gallery-item
+         [:a {:href "http://gstemnams.weebly.com/"}
           [:img {:src "img/gallery/girls-in-stem.jpg"}]
-          [:div.label "Girls in STEM"]]]]
+          [:div.label "Girls in STEM"]]]]]
 
     [:div.gallery-section
       [:h2 [:mark "Talks + Performances"]]
 
       [:div.gallery-row
         [:div.gallery-item
+         [:a {:href "https://youtu.be/tnKPuoWniTg?list=PLWYAdh9hA7geMRCgwDomlHAdwlsno32LU"}
           [:img {:src "img/gallery/moonconf.jpg"}]
-          [:div.label "MoonConf"]]
+          [:div.label "MoonConf"]]]
         [:div.gallery-item
+         [:a {:href "https://mmmanyfold.wordpress.com/tag/denver-public-schools/"}
           [:img {:src "img/gallery/north-hs.gif"}]
-          [:div.label "Denver Public Schools"]]
+          [:div.label "Denver Public Schools"]]]
         [:div.gallery-item
+         [:a {:href "https://mmmanyfold.wordpress.com/2017/02/06/sonic-pi-shadertone-owl-tech-fair/"}
           [:img {:src "img/gallery/owl-tech-fair.jpg"}]
-          [:div.label "OWL Tech Fair"]]
+          [:div.label "OWL Tech Fair"]]]
         [:div.gallery-item
+         [:a {:href "https://www.instagram.com/p/BKb_PKsBio4/?taken-by=eemshi"}
           [:img {:src "img/gallery/signal-to-noise.gif"}]
-          [:div.label "Signal-to-Noise Studios"]]]]])
+          [:div.label "Signal-to-Noise Studios"]]]]]])
