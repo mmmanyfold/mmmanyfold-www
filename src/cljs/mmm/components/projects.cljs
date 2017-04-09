105447493281461e4a6ab0d21dd046b776660176
eemshi <oreneemshi@gmail.com>
2017-04-09 14:47:20 -0600

    act-three: rainbow rabbit

--- src/cljs/mmm/components/projects.cljs
+++ src/cljs/mmm/components/projects.cljs
@@ -5,21 +5,20 @@
             [cljsjs.photoswipe-ui-default]
             [reagent.core :as reagent :refer [atom]]))
 
-(defn project [title img summary info width height]
-   [:figure.gallery-item
-    {:item-prop  "associatedMedia"
-     :item-scope "true"
-     :item-type  "http://schema.org/ImageObject"}
-    [:a {:href img
-         :item-prop "contentUrl"
-         :data-size (str width "x" height)}
-     [:img {:src img :item-prop "thumbnail"}]
-     [:div.image-label title]]
-    [:figcaption {:item-prop "caption description"
-                  :width (str width "px")}
-     [:div [:h2.title title
-            [:p.summary summary]
-            info]]]])
+(defn project [title img summary width height]
+ [:figure.gallery-item
+  {:item-prop  "associatedMedia"
+   :item-scope "true"
+   :item-type  "http://schema.org/ImageObject"}
+  [:a {:href img
+       :item-prop "contentUrl"
+       :data-size (str width "x" height)}
+   [:img {:src img :item-prop "thumbnail"}]
+   [:div.image-label title]]
+  [:figcaption {:item-prop "caption description"
+                :width (str width "px")}
+   [:div [:h1.title title]
+         summary]]])
 
 (defn full-screen-gallery []
   [:div.pswp {:tabIndex "-1" :role "dialog" :aria-hidden "true"}
@@ -62,93 +61,78 @@
              [project
               "Playground Coffee Shop"
               "img/gallery/playground.jpg"
-              "Custom website for Brooklyn coffee shop, venue, and consignment store."
-              [:div.info
+              [:h2
+               [:p "Custom website for Brooklyn coffee shop, venue, and consignment store."]
                [:a {:class "rabbit"
                     :href "http://playgroundcoffeeshop.com"} "playgroundcoffeeshop.com"]
-               [:p [:i "mmmanyfold API, Clojure(script), re-frame, Mailgun, Contentful, AWS"]]]
+               [:p.tech "mmmanyfold API, Clojure(script), re-frame, Mailgun, Contentful, AWS"]]
               "553"
               "400"]
 
              [project
               "Sporting Life"
               "img/gallery/sporting-life.gif"
-              "Custom website for NYC producer Sporting Life."
-              [:div.info
+              [:h2
+               [:p "Custom website for NYC producer Sporting Life."]
                [:a {:class "rabbit"
                     :href "http://sportinglife.nyc"} "sportinglife.nyc"]
-               [:p [:i "Github Pages"]]]
+               [:p.tech "Github Pages"]]
               "640"
               "429"]
 
              [project
               "Princess Nokia"
               "img/gallery/princess-nokia.jpg"
-              "Custom website for NYC artist Princess Nokia."
-              [:div.info
+              [:h2
+               [:p "Custom website for NYC artist Princess Nokia."]
                [:a {:class "rabbit"
                     :href "http://princessnokia.org"} "princessnokia.org"]
-               [:p [:i "mmmanyfold API, Clojure, Node.js, React.js, AWS"]]]
+               [:p.tech "mmmanyfold API, Clojure, Node.js, React.js, AWS"]]
               "425"
               "405"]
 
              [project
               "Dizzy Magazine"
               "img/gallery/dizzy.jpg"
-              "Custom website for Dizzy Magazine."
-              [:div.info
+              [:h2
+               [:p "Custom website for Dizzy Magazine."]
                [:a {:class "rabbit"
                     :href "http://www.dizzymagazine.com"} "dizzymagazine.com"]
-               [:p [:i "mmmanyfold API, Clojure(script), re-frame, AWS"]]]
+               [:p.tech "mmmanyfold API, Clojure(script), re-frame, AWS"]]
               "221"
               "286"]
 
              [project
               "OWLET"
               "img/gallery/owlet.png"
-              "Web platform designed for middle schoolers to explore creative tech via self-guided activities."
-              [:div.info
+              [:h2
+               [:p "Web platform designed for middle schoolers to explore creative tech via self-guided activities."]
                [:a {:class "rabbit"
                     :href "http://owlet.codefordenver.org"} "owlet.codefordenver.org"]
-               [:p [:i "Owlet API, Clojure(script), re-frame, PostgreSQL, Contentful, Auth0, Firebase, AWS"]]]
+               [:p.tech "Owlet API, Clojure(script), re-frame, PostgreSQL, Contentful, Auth0, Firebase, AWS"]]
               "588"
               "588"]
 
              [project
               "Picture Room"
               "img/gallery/picture-room.png"
-              "Custom website/online shop for Picture Room."
-              [:div.info
+              [:h2
+               [:p "Custom website/online shop for Picture Room."]
                [:a {:class "rabbit"
                     :href "https://pictureroom.shop"} "pictureroom.shop"]
-               [:p [:i "Lightspeed eCom, Github Pages, mmmanyfold API, Facebook Graph API"]]]
+               [:p.tech "Lightspeed eCom, Github Pages, mmmanyfold API, Facebook Graph API"]]
               "665"
               "525"]
 
              [project
               "Letter Racer"
               "img/gallery/letter-racer.gif"
-              "Custom website/online shop for the NYC music + art collective."
-              [:div.info
+              [:h2
+               [:p "Custom website/online shop for the NYC music + art collective."]
                [:a {:class "rabbit"
                     :href "http://letterracer.com"} "letterracer.com"]
-               [:p [:i "Shopify, Cart.js, Jekyll, Github Pages, AWS"]]]
+               [:p.tech "Shopify, Cart.js, Jekyll, Github Pages, AWS"]]
               "640"
               "500"]]
 
            (full-screen-gallery)]])}))
-
-
-; [project :8
-;  "Denver Art Museum"
-;  "img/gallery/design-challenge.gif"
-;  "A design summer camp for ages 9-12, reimagined each year."
-;  [:div.info
-;   [:a {:href "design-challenge.html"} "Design Challenge 2016"]]]]
-;
-; [project :9
-;  "Girls in STEM"
-;  "img/gallery/girls-in-stem.jpg"
-;  "Denver area middle school after-school program introducing girls to women in STEM."
-;  [:div.info
-;   [:a {:href "http://gstemnams.weebly.com/"} "GSTEM @ North Arvada"]]]]]]]
--- src/cljs/mmm/components/talks.cljs
+++ src/cljs/mmm/components/talks.cljs
@@ -5,7 +5,7 @@
             [cljsjs.photoswipe-ui-default]
             [reagent.core :as reagent :refer [atom]]))
 
-(defn talk [title img summary info width height]
+(defn talk [title img summary width height]
  [:figure.gallery-item
   {:item-prop  "associatedMedia"
    :item-scope "true"
@@ -17,37 +17,35 @@
    [:div.image-label title]]
   [:figcaption {:item-prop "caption description"
                 :width (str width "px")}
-   [:div [:h2.title title]
-         [:p.summary summary]
-         info]]])
+   [:div [:h1.title title]
+         summary]]])
 
 
 (defn full-screen-gallery []
-  [:div
-   [:div.pswp {:tabIndex "-1" :role "dialog" :aria-hidden "true"}
-    [:div.pswp__bg]
-    [:div.pswp__scroll-wrap
-     [:div.pswp__container
-      [:div.pswp__item]
-      [:div.pswp__item]
-      [:div.pswp__item]]
-     [:div.pswp__ui.pswp__ui--hidden
-      [:div.pswp__top-bar
-       [:div.pswp__counter]
-       [:button.pswp__button.pswp__button--close {:title "Close (Esc)"}]
-       [:button.pswp__button.pswp__button--share {:title "Share"}]
-       [:button.pswp__button.pswp__button--fs {:title "Toggle fullscreen"}]
-       [:button.pswp__button.pswp__button--zoom {:title "Zoom in/out"}]
-       [:div.pswp__preloader
-        [:div.pswp__preloader__icn
-         [:div.pswp__preloader__cut
-          [:div.pswp__preloader__donut]]]]]
-      [:div.pswp__share-modal.pswp__share-modal--hidden.pswp__single-tap
-       [:div.pswp__share-tooltip]]
-      [:button.pswp__button.pswp__button--arrow--left {:title "Previous (arrow left)"}]
-      [:button.pswp__button.pswp__button--arrow--right {:title "Next (arrow right)"}]
-      [:div.pswp__caption
-       [:div.pswp__caption__center]]]]]])
+ [:div.pswp {:tabIndex "-1" :role "dialog" :aria-hidden "true"}
+  [:div.pswp__bg]
+  [:div.pswp__scroll-wrap
+   [:div.pswp__container
+    [:div.pswp__item]
+    [:div.pswp__item]
+    [:div.pswp__item]]
+   [:div.pswp__ui.pswp__ui--hidden
+    [:div.pswp__top-bar
+     [:div.pswp__counter]
+     [:button.pswp__button.pswp__button--close {:title "Close (Esc)"}]
+     [:button.pswp__button.pswp__button--share {:title "Share"}]
+     [:button.pswp__button.pswp__button--fs {:title "Toggle fullscreen"}]
+     [:button.pswp__button.pswp__button--zoom {:title "Zoom in/out"}]
+     [:div.pswp__preloader
+      [:div.pswp__preloader__icn
+       [:div.pswp__preloader__cut
+        [:div.pswp__preloader__donut]]]]]
+    [:div.pswp__share-modal.pswp__share-modal--hidden.pswp__single-tap
+     [:div.pswp__share-tooltip]]
+    [:button.pswp__button.pswp__button--arrow--left {:title "Previous (arrow left)"}]
+    [:button.pswp__button.pswp__button--arrow--right {:title "Next (arrow right)"}]
+    [:div.pswp__caption
+     [:div.pswp__caption__center]]]]])
 
 (defn talks-component []
   (reagent/create-class
@@ -64,47 +62,55 @@
             [talk
              "OWL Tech Fair"
              "img/gallery/owl-tech-fair.jpg"
-             "Audio/visual code art performance + workshop for Denver youth."
-             [:div.info
-              [:p [:a {:href "http://blog.mmmanyfold.com/2017/02/06/sonic-pi-shadertone-owl-tech-fair/"} "Photos"]]
-              [:p [:i "Clojure, Shadertone, Sonic Pi"]]]
+             [:h2
+              [:p "Audio/visual code art performance + workshop for Denver youth."]
+              [:a {:class "rabbit"
+                   :href "http://blog.mmmanyfold.com/2017/02/06/sonic-pi-shadertone-owl-tech-fair/"}
+                "Photos"]
+              [:p.tech "Clojure, Shadertone, Sonic Pi"]]
              "400"
              "430"]
             [talk
              "MoonConf"
              "img/gallery/moonconf.png"
-             [:span "Watch: " [:a {:href "https://youtu.be/tnKPuoWniTg?list=PLWYAdh9hA7geMRCgwDomlHAdwlsno32LU"} "Hacking Web Apps with Clojure(Script) @ Code for Denver"]]
-             [:div.info
+             [:h2
+              [:p "Watch: " [:a {:class "rabbit"
+                                 :href "https://youtu.be/tnKPuoWniTg?list=PLWYAdh9hA7geMRCgwDomlHAdwlsno32LU"} "Hacking Web Apps with Clojure(Script) @ Code for Denver"]]
               [:p "Presented at MoonConf 2016 by Shelby Switzer & David A. Viramontes"]
-              [:p "We ♥ " [:a {:href "http://moonconf.org/"} "MoonConf"]]]
+              [:p "We ♥ " [:a {:class "rabbit"
+                               :href "http://moonconf.org/"} "MoonConf"]]]
              "277"
              "163"]
             [talk
              "Code for Denver"
              "img/gallery/lisp.png"
-             [:a {:href "https://codefordenver.github.io/tutorials/intro-to-cljs/"} "Intro to Clojure(script)"]
-             [:div.info
+             [:h2
+              [:p [:a {:class "rabbit"
+                       :href "https://codefordenver.github.io/tutorials/intro-to-cljs/"}
+                    "Intro to Clojure(script)"]]
               [:p "Presented at Code for Denver by Michelle Lim & Tyler Perkins"]]
              "252"
              "211"]
             [talk
              "Denver Public Schools"
              "img/gallery/north-hs.gif"
-             [:span
-              [:a {:href "http://blog.mmmanyfold.com/tag/denver-public-schools/"}
-               "Code art demos "]
-              "in Denver area public schools."]
-             [:div.info
-              [:p [:i "Clojure, mmmanyfold API, Giphy API, Facebook API, Indico, VVVV"]]]
+             [:h2
+              [:p [:a {:class "rabbit"
+                       :href "http://blog.mmmanyfold.com/tag/denver-public-schools/"}
+                   "Code art demos "]
+                 "in Denver area public schools."]
+              [:p.tech "Clojure, mmmanyfold API, Giphy API, Facebook API, Indico, VVVV"]]
              "268"
              "298"]
             [talk
              "Signal-to-Noise Media Lab"
              "img/gallery/signal-to-noise.gif"
-             "Audio/visual code art performance during Denver Startup Week 2016."
-             [:div.info
-              [:p [:a {:href "https://www.denverstartupweek.org/panel-picker/2399-an-amalgamation-of-art-science-and-technology"} "Event details"]]
-              [:p [:i "Clojure, Shadertone, Sonic Pi"]]]
+             [:h2
+              [:p "Audio/visual code art performance during Denver Startup Week 2016."]
+              [:a {:class "rabbit"
+                   :href "https://www.denverstartupweek.org/panel-picker/2399-an-amalgamation-of-art-science-and-technology"}
+                "Event details"]
+              [:p.tech "Clojure, Shadertone, Sonic Pi"]]
              "640"
              "454"]]
 
