39c8480d7901681c36613e86b78824ca5b3438b5
David A. Viramontes <dviramontes@gmail.com>
2017-08-27 15:07:52 -0600

    fix: profile css (#57)

--- src/cljs/mmm/components/profile.cljs
+++ src/cljs/mmm/components/profile.cljs
@@ -5,7 +5,7 @@
    [:a.db.link.dim.tc.rabbit {:href url :target "_blank"}
     (if-not (empty? imageUrl)
       [:img.w-100.db.outline.black-10 {:src imageUrl}]
-      [:div.space-invader])
+      [:img.space-invader {:src "https://s3.amazonaws.com/mmm-statics/space-invader.png"}])
     [:dl.mt2.f6.lh-copy.bg-purple
      [:dt.clip "title"]
      [:dd.ml0.black.truncate.w-100.bg-light-green title]
@@ -13,7 +13,7 @@
      [:dd.ml0.gray.truncate.w-100.bg-light-green description]]]])
 
 (defn embed-url [{:keys [title description url]}]
-  [:div.fl.w-50.w-25-m.w-50-l.pa2
+  [:div.fl.w-50.w-25-m.w-20-l.pa2
    [:a.db.link.dim.tc.rabbit {:href url :target "_blank"}
     [:iframe.embed {:src url :frame-border 0 :scrolling "no"}]
     [:dl.mt2.f6.lh-copy.bg-purple
@@ -23,7 +23,7 @@
      [:dd.ml0.gray.truncate.w-100.bg-light-green description]]]])
 
 (defn embed-code [{:keys [title description content url]}]
-  [:div.fl.w-50.w-25-m.w-40-l.pa2
+  [:div.fl.w-50.w-25-m.w-20-l.pa2
    [:a.db.link.dim.tc.rabbit {:href url :target "_blank"}
     [:div {:dangerouslySetInnerHTML {:__html content}}]
     [:dl.mt2.f6.lh-copy.bg-purple
