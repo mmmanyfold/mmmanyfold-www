bcdd41c256893e5989773f42ffb256441726abd4
David A. Viramontes <dviramontes@gmail.com>
2018-09-02 17:09:41 -0500

    wip: begin deployment process

--- API/gatsby-config.js
+++ API/gatsby-config.js
@@ -1,6 +1,17 @@
+const { env } = process;
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: env['MMM_CONTENTFUL_SERVER_SPACE_ID'],
+        accessToken: env['MMM_CONTENTFUL_SERVER_ACCESS_TOKEN'],
+      },
+    },
+  ],
+};
