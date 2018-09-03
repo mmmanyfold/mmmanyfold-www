282cd2fbefb34c9dfdf7f7d8bd90c0801d04bb70
eemshi <oreneemshi@gmail.com>
2018-09-02 21:46:48 -0600

    create project gallery rosettes

--- src/cljs/mmm/views/home.cljs
+++ src/cljs/mmm/views/home.cljs
@@ -27,43 +27,62 @@
 		}
 	}")
 
-(def projects [{:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["art"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["art"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["art"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["art"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["art"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["art"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["online shop"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["online shop"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["business", "online shop"]},
-               {:title "project 1"
-                :cover "img/projects/tony-seltzer.gif"
-                :category ["education", "nonprofit"]}])
+(def all-projects [{:title "project 1"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["recent"]},
+                   {:title "project 2"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["recent"]},
+                   {:title "project 3"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["recent"]},
+                   {:title "project 4"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["recent"]},
+                   {:title "project 5"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["recent"]},
+                   {:title "project 6"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["recent"]},
+                   {:title "project 7"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["past"]},
+                   {:title "project 8"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["past"]},
+                   {:title "project 9"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["past"]},
+                   {:title "project 10"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["past"]}
+                   {:title "project 11"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["past"]}
+                   {:title "project 12"
+                    :cover "img/projects/tony-seltzer.gif"
+                    :category ["past"]}])
+
+(def colors ["#fbb4ae"   ;red
+             "#fed9a6"   ;orange
+             "#ccebc5"   ;green
+             "#b3cde3"   ;blue
+             "#f2f2f2"    ;white
+             "#decbe4"   ;purple
+             "#b3cde3"   ;blue
+             "#f2f2f2"   ;white
+             "#decbe4"   ;purple
+             "#fbb4ae"   ;red
+             "#fed9a6"   ;orange
+             "#ccebc5"]) ;green
 
 (defn filter-by-cat [cat]
   (filter (fn [p]
             (some
               #(when (= % cat) %)
               (:category p)))
-    projects))
+    all-projects))
 
 (defn home-view []
   ;(let [db-key :projects]                                    ;; 0. declare unique db-key
@@ -71,21 +90,20 @@
   ;  (rf/dispatch [:get-contentful-data db-key query :media]) ;; 2. retrieve contentful data & pass key for assoc in db
   ;  (if-let [projects (rf/subscribe [db-key])]
   ;    (prn projects)
-  (let [categories ["art"
-                    "business"
-                    "online shop"
-                    "nonprofit"
-                    "education"
-                    "installation"
-                    "mobile"]]
+  (let [categories ["recent"
+                    "past"]]
     [:div
-     [:div
+     [:div.projects-gallery
       (for [cat categories
             :let [projects (filter-by-cat cat)]]
         ^{:key (gensym)}
-        [:div
+        [:div.category-wrap
          [:h1 cat]
-         (for [project projects
-               :let [{:keys [title cover category]} project]]
-           ^{:key (gensym)}
-           [bubble :blue title])])]]))
+         [:div.category-projects
+
+          (for [project projects
+                :let [{:keys [title cover]} project
+                      n (.indexOf all-projects project)
+                      color (nth colors n)]]
+            ^{:key (gensym)}
+            [bubble color cover title])]])]]))
